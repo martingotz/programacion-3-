40b64edfca9e0951dd6acff4ca68bd1c14adee7f
martingotz <gotzm@udesa.edu.ar>
2021-09-09 10:52:46 -0300

    cambios add

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Movies from '../Movies/Movies';
 
 class Card extends Component {
+    
     render() {
         return (
             <div>
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -6,9 +6,9 @@ import React, { Component } from 'react';
 
          )
          this.state ={ 
-             //moviesOriginal: [],
-           //  moviesFilter:[],
-           //  nexturl:"",
+            moviesOriginal: [],
+            moviesFilter:[],
+            nexturl:"",
          }
      }
      componentDidMount(){
@@ -21,11 +21,11 @@ import React, { Component } from 'react';
            
            console.log('datos de la api');
            console.log(data);
-           //this.setState({
-              // moviesOriginal: data.results,// esto esta mal, cambiar cuando traiga ok la api.
-              // moviesFilter: data.results,
-              // nexturl: data.info.next,
-          // })
+           this.setState({
+               moviesOriginal: data.results,// esto esta mal, cambiar cuando traiga ok la api.
+               moviesFilter: data.results,
+               nexturl: data.info.next, // Va con agregar mas 
+           })
 
 
        })
@@ -34,12 +34,35 @@ import React, { Component } from 'react';
      componentDidUpdate(){
          console.log("componentDidUpdate");
      }
+    add (){
+        const url = this.state.nexturl;
+        fetch(url)
+        .then((respuesta)=> respuesta.json())
+        .then((data)=>{
+            console.log(data);
+            this.setState({
+                nexturl:data.info.next,
+                personajes: this.state.personajes.concat(data.results),
+            });
+        })
+        .catch((e)=> console.log(e))
+    }
+    original(){
+        this.setState({
+            moviesFilter: this.state.moviesOriginal,
+            nexturl: 'https://api.themoviedb.org/3/movie/popular?api_key=4376257ca33773b58ce7e3a2ca8c1180&language=en-US&page=2'
+        })
+
+    }
+
         
     render() {
         console.log("renderizado");
         return (
             <div>
                 <p>Esto es movies </p>
+                <button onClick={()=>this.add()} > Add More </button>
+                <button onClick={()=>this.original()}>Limit</button>
             </div>
         )
     }
