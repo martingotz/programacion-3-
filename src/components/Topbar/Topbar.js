47218d703c64c741eeed8b10f49755fe306989d7
SantinoBonarrigo <>
2021-09-14 21:46:48 -0300

    menus

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -3,17 +3,47 @@ import './Topbar.css';
 
 
  class Topbar extends Component {
+
+
+constructor(){
+
+super();
+
+this.state = {  
+
+view: false
+
+}  }
+
+
+horizontal(){ 
+
+}
+
+vertical(){ 
+
+}
+
+
+
+
+
+
+
     render() {
         return (
             <>
+            <p className="orden">Ordenar ASC/ DESC</p>
             <div className="TopbarContainer">
 
                 
 
                 
-                <p className="orden">Ordenar ASC/ DESC</p>
-                <i className="fas fa-th"></i>
-                <i className="fas fa-align-justify"></i>
+                {/* <i className="fas fa-th"><img className="leftarrow" src={"/assets/img/menu.jpeg"}/></i> ver desp si se puede poner un button sin sacar i 
+                <i className="fas fa-align-justify"><img className="leftarrow" src={"/assets/img/menu2.jpeg"}/></i> */}
+
+                <button onClick={()=>this.horizontal()} className="fas fa-th"><img className="leftarrow" src={"/assets/img/menu.jpeg"}/></button>
+                <button onClick={()=>this.vertical()} className="fas fa-align-justify"><img className="leftarrow" src={"/assets/img/menu2.jpeg"}/></button>
                 <form action="">
                 <input type="text" name="search" id="" placeholder="Search" />
                     <button type="submit">Enviar<i className="fas fa-search"></i></button>
