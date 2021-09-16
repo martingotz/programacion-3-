f7989a66bac90cc39d31ea8249ae841ae75e906f
martingotz <gotzm@udesa.edu.ar>
2021-09-16 10:30:02 -0300

    header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -13,8 +13,10 @@ return(
 
             <section>
             <p className="orden">Ordenar ASC/ DESC</p>
-                <i className="fas fa-th"></i>
-                <i className="fas fa-align-justify"></i>
+            <button onClick={()=>this.horizontal()} className="fas fa-th"><img className="leftarrow" src={"/assets/img/menu.jpeg"}/></button>
+                <button onClick={()=>this.vertical()} className="fas fa-align-justify"><img className="leftarrow" src={"/assets/img/menu2.jpeg"}/></button>
+                {/*<i className="fas fa-th"></i>
+                <i className="fas fa-align-justify"></i>*/}
                 <form action="">
                 <input type="text" name="search" id="" placeholder="Search" />
                     <button type="submit">Enviar<i className="fas fa-search"></i></button>
